0cda3c865fd8cb35c6c2af46065242c7460fcb2a
Noe-Chavez <noe_fam_chavez@yahoo.com.mx>
2020-08-07 11:01:48 -0500

    data class Repo adding.

---
+++ app/src/main/java/com/disoftware/model/Contributor.kt
@@ -0,0 +1,27 @@
+package com.disoftware.model
+
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import com.google.gson.annotations.SerializedName
+
+@Entity(
+    primaryKeys = ["repoName", "repoOwner", "login"],
+    foreignKeys = [ForeignKey(
+        entity = Repo::class,
+        parentColumns = ["name", "owner_login"],
+        childColumns = ["repoName", "repoOwner"],
+        onUpdate = ForeignKey.CASCADE
+    )]
+)
+data class Contributor(
+    @field:SerializedName("login")
+    val login: String,
+    @field:SerializedName("contributions")
+    val contributions: Int,
+    @field:SerializedName("avatar_url")
+    val avatarUrl: String
+) {
+    // Variables asociadas a repo.kt con relación a primaryKeys = ["name", "owner_login"].
+    lateinit var repoName: String // guarda el nombre del repo al cual pertenece esta contribucion.
+    lateinit var repoOwner: String // prpietario del repo al que se esta contribuyendo.
+}
