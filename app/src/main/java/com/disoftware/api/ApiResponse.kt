3f4866b5a617683005525e23cd2bf3846d1ea229
Noe-Chavez <noe_fam_chavez@yahoo.com>
2020-08-17 19:38:16 -0500

    ApiResponse.kt class completed

--- app/src/main/java/com/disoftware/api/ApiResponse.kt
+++ app/src/main/java/com/disoftware/api/ApiResponse.kt
@@ -1,11 +1,41 @@
 package com.disoftware.api
 
+import retrofit2.Response
+import java.lang.NumberFormatException
 import java.util.regex.Matcher
 import java.util.regex.Pattern
 
 sealed class ApiResponse<T> {
+    companion object {
+        fun<T> create(error: Throwable): ApiErrorResponse<T> {
+            // Cuando halla un error...
+            return ApiErrorResponse(error.message?: "unknown error")
+        }
 
+        // En caso de que llege una respuesta.
+        fun<T> create(response: Response<T>): ApiResponse<T> {
+            return if (response.isSuccessful) {
+                val body:T? = response.body()
+                if (body == null || response.code() == 204) {
+                    ApiEnptyResponse()
+                } else {
+                    ApiSucessResponse(body = body, linksHeaders = response.headers()?.get("link"))
+                }
+            } else {
+                val msg: String? = response.errorBody()?.string()
+                val errorMsg: String = if (msg.isNullOrEmpty()) {
+                    response.message()
+                } else {
+                    msg
+                }
+                ApiErrorResponse(errorMsg?: "unknow error")
+            }
+        }
+    }
 }
+
+// Para el error 104, cuando se procesa una petición, pero no devuelve ningún body.
+class ApiEnptyResponse<T>: ApiResponse<T>()
 
 data class ApiSucessResponse<T> (
     val body: T,
@@ -15,6 +45,23 @@ data class ApiSucessResponse<T> (
         body = body,
         links = linksHeaders?.extractLinks()?: emptyMap()
     )
+
+    // Recuperar la siguinete página
+    val nextPage: Int? by lazy (LazyThreadSafetyMode.NONE) {
+        links[NEXT_LINK]?.let {
+            next ->
+            val matcher: Matcher = PAGE_PATTERN.matcher(next)
+            if (!matcher.find() || matcher.groupCount() != 1) { //nexy sólo llega un unico valor por lo que este valor es un error.
+                null
+            } else {
+              try {
+                  Integer.parseInt(matcher.group(1))
+              }  catch (ex: NumberFormatException) {
+                  null
+              }
+            }
+        }
+    }
 
     companion object {
         private val LINK_PATTERN = Pattern.compile("<([^>]*)>[\\s]*;[\\s]*rel=\"([a-zA-Z0-9]+)\"")
@@ -34,3 +81,8 @@ data class ApiSucessResponse<T> (
     }
 
 }
+
+// Clase para los errores.
+data class ApiErrorResponse<T> (
+    val errorMessage: String
+): ApiResponse<T>()
