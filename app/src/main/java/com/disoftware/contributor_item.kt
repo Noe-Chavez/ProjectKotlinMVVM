207aa0c6ef07bd55f2a904b163952456ea038c18
Noe-Chavez <noe_fam_chavez@yahoo.com.mx>
2020-08-30 23:10:21 -0500

    project finished (whit erros).

--- app/src/main/java/com/disoftware/contributor_item.kt
+++ app/src/main/java/com/disoftware/contributor_item.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class contributor_item : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_contributor_item)
+    setContentView(R.layout.contributor_item)
 }
 }
